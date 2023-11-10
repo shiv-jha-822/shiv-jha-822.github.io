a06e88a796c3058e0d4adc34becc766074d665e9
Shivam Jha <shivjha@berkeley.edu>
2023-11-09 17:12:27 -0800

    finished hw 8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleSongs() {
+    songsSecondHalf = document.getElementById("songs-second-half");
+    songsSecondHalf.classList.toggle("hidden");
+}
+
+document.getElementById("toggle-button").onclick = toggleSongs;
